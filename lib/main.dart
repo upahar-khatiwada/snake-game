9457411dbfb17c4ea450b4670e8921a2ed346ff7
upahar windows <upaharlol@gmail.com>
2025-06-29 18:49:43 +0545

    Snake Game completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:snake_game/enums.dart';
 import 'dart:async';
 
 void main() {
-  runApp(const SnakeGame());
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false, home: SnakeGame()),
+  );
 }
 
 class SnakeGame extends StatefulWidget {
@@ -17,14 +18,15 @@ class SnakeGame extends StatefulWidget {
 
 class _SnakeGameState extends State<SnakeGame> {
   bool canPauseNow = false;
+  bool isOut = false;
   // Direction dir = Direction.values[Random().nextInt(Direction.values.length)];
-  Timer? gameTimer;
 
   Direction dir = Direction.up;
   List<int> snakeArray = <int>[61, 81, 101];
   late int totalGridCount;
   late int foodPosition;
   late List<int> availableFoodPositions;
+  int currentScore = 0;
 
   @override
   void didChangeDependencies() {
@@ -41,9 +43,7 @@ class _SnakeGameState extends State<SnakeGame> {
   }
 
   void startGame() {
-    gameTimer = Timer.periodic(const Duration(milliseconds: 300), (
-      Timer timer,
-    ) {
+    Timer.periodic(const Duration(milliseconds: 250), (Timer timer) {
       if (!canPauseNow) {
         timer.cancel();
       } else {
@@ -52,45 +52,108 @@ class _SnakeGameState extends State<SnakeGame> {
     });
   }
 
-  void snakeMovement() {
+  Future<void> gameOver() async {
     setState(() {
-      switch (dir) {
-        case Direction.up:
-          if (snakeArray.first < 20) {
-            snakeArray = snakeArray.map((int pos) {
-              return pos + totalGridCount - 20;
-            }).toList();
-          } else {
-            snakeArray = snakeArray.map((int pos) {
-              return pos - 20;
-            }).toList();
-          }
-          break;
-        case Direction.down:
-          if (snakeArray.first > (totalGridCount - 20)) {
-            snakeArray = snakeArray
-                .map((int pos) => pos - totalGridCount + 20)
-                .toList();
-          } else {
-            snakeArray = snakeArray.map((int pos) => pos + 20).toList();
-          }
-          break;
-        case Direction.left:
-          if ((snakeArray.first) % 20 == 0) {
-            snakeArray = snakeArray.map((int pos) => pos + 20 - 1).toList();
-          } else {
-            snakeArray = snakeArray.map((int pos) => pos - 1).toList();
-          }
-          break;
-        case Direction.right:
-          if ((snakeArray.first + 1) % 20 == 0) {
-            snakeArray = snakeArray.map((int pos) => pos - 20 + 1).toList();
-          } else {
-            snakeArray = snakeArray.map((int pos) => pos + 1).toList();
-          }
-          break;
-      }
+      isOut = true;
     });
+
+    if (mounted) {
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Game Over!!!'),
+            content: Text('Score: $currentScore'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+
+                  setState(() {
+                    currentScore = 0;
+                    isOut = false;
+                    canPauseNow = true;
+
+                    dir = Direction.up;
+                    snakeArray = <int>[61, 81, 101];
+
+                    availableFoodPositions = List<int>.generate(
+                      totalGridCount,
+                      (int index) => index,
+                    ).where((int pos) => !snakeArray.contains(pos)).toList();
+
+                    foodPosition =
+                        availableFoodPositions[Random().nextInt(
+                          availableFoodPositions.length,
+                        )];
+                  });
+
+                  startGame();
+                },
+                child: const Text('Play Again!'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void snakeMovement() {
+    if (!isOut) {
+      setState(() {
+        int head;
+        switch (dir) {
+          case Direction.up:
+            head = (snakeArray.first < 20)
+                ? snakeArray.first + totalGridCount - 20
+                : snakeArray.first - 20;
+            break;
+
+          case Direction.down:
+            head = (snakeArray.first >= totalGridCount - 20)
+                ? snakeArray.first - totalGridCount + 20
+                : snakeArray.first + 20;
+            break;
+
+          case Direction.left:
+            head = (snakeArray.first % 20 == 0)
+                ? snakeArray.first + 19
+                : snakeArray.first - 1;
+            break;
+
+          case Direction.right:
+            head = ((snakeArray.first + 1) % 20 == 0)
+                ? snakeArray.first - 19
+                : snakeArray.first + 1;
+            break;
+        }
+
+        if (snakeArray.contains(head)) {
+          isOut = true;
+          gameOver();
+          return;
+        }
+
+        snakeArray.insert(0, head);
+
+        if (head == foodPosition) {
+          currentScore += 1;
+
+          availableFoodPositions = List<int>.generate(
+            totalGridCount,
+            (int index) => index,
+          ).where((int pos) => !snakeArray.contains(pos)).toList();
+
+          foodPosition =
+              availableFoodPositions[Random().nextInt(
+                availableFoodPositions.length,
+              )];
+        } else {
+          snakeArray.removeLast();
+        }
+      });
+    }
   }
 
   @override
@@ -98,91 +161,91 @@ class _SnakeGameState extends State<SnakeGame> {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
     final int totalGridCount = (screenHeight * 0.845).toInt();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
         backgroundColor: Colors.grey,
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          title: const Text('Score: 10', style: TextStyle(color: Colors.black)),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  canPauseNow = !canPauseNow;
-                  startGame();
-                });
-              },
-              icon: canPauseNow
-                  ? const Icon(Icons.pause)
-                  : const Icon(Icons.play_arrow),
-            ),
-          ],
+        title: Text(
+          'Score: $currentScore',
+          style: const TextStyle(color: Colors.black),
         ),
-        body: Padding(
-          padding: EdgeInsets.only(
-            right: screenWidth * 0.03,
-            left: screenWidth * 0.03,
-            bottom: screenHeight * 0.07,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              setState(() {
+                canPauseNow = !canPauseNow;
+                startGame();
+              });
+            },
+            icon: canPauseNow
+                ? const Icon(Icons.pause)
+                : const Icon(Icons.play_arrow),
           ),
-          child: GestureDetector(
-            onVerticalDragUpdate: (DragUpdateDetails details) {
-              if (dir != Direction.up && details.delta.dy > 0) {
-                dir = Direction.down;
-              }
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(
+          right: screenWidth * 0.03,
+          left: screenWidth * 0.03,
+          bottom: screenHeight * 0.07,
+        ),
+        child: GestureDetector(
+          onVerticalDragUpdate: (DragUpdateDetails details) {
+            if (dir != Direction.up && details.delta.dy > 0) {
+              dir = Direction.down;
+            }
 
-              if (dir != Direction.down && details.delta.dy < 0) {
-                dir = Direction.up;
-              }
-            },
-            onHorizontalDragUpdate: (DragUpdateDetails details) {
-              if (dir != Direction.left && details.delta.dx > 0) {
-                dir = Direction.right;
-              }
+            if (dir != Direction.down && details.delta.dy < 0) {
+              dir = Direction.up;
+            }
+          },
+          onHorizontalDragUpdate: (DragUpdateDetails details) {
+            if (dir != Direction.left && details.delta.dx > 0) {
+              dir = Direction.right;
+            }
 
-              if (dir != Direction.right && details.delta.dx > 0) {
-                dir = Direction.left;
-              }
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                border: BoxBorder.all(color: Colors.black38),
+            if (dir != Direction.right && details.delta.dx < 0) {
+              dir = Direction.left;
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: BoxBorder.all(color: Colors.black38),
+            ),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 20,
               ),
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 20,
-                ),
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: totalGridCount,
-                itemBuilder: (BuildContext context, int index) {
-                  if (snakeArray.contains(index)) {
-                    if (snakeArray.first == index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(0.5),
-                        child: Container(color: Colors.red),
-                      );
-                    }
-
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: totalGridCount,
+              itemBuilder: (BuildContext context, int index) {
+                if (snakeArray.contains(index)) {
+                  if (snakeArray.first == index) {
                     return Padding(
                       padding: const EdgeInsets.all(0.5),
-                      child: Container(color: Colors.black),
-                    );
-                  }
-
-                  if (index == foodPosition) {
-                    return Padding(
-                      padding: const EdgeInsets.all(0.5),
-                      child: Container(color: Colors.green),
+                      child: Container(color: Colors.red),
                     );
                   }
 
                   return Padding(
                     padding: const EdgeInsets.all(0.5),
-                    child: Container(color: Colors.white),
+                    child: Container(color: Colors.black),
                   );
-                },
-              ),
+                }
+
+                if (index == foodPosition) {
+                  return Padding(
+                    padding: const EdgeInsets.all(0.5),
+                    child: Container(color: Colors.green),
+                  );
+                }
+
+                return Padding(
+                  padding: const EdgeInsets.all(0.5),
+                  child: Container(color: Colors.white),
+                );
+              },
             ),
           ),
         ),
